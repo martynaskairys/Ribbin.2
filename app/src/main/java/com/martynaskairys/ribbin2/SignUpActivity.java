2375a75a6c2b4f98b0aaa2ffc377b893f77eac1b
martynaskairys <martynaskairys@gmail.com>
2015-12-05 20:28:44 +0200

    Initial commit

---
+++ app/src/main/java/com/martynaskairys/ribbin2/SignUpActivity.java
@@ -0,0 +1,22 @@
+package com.martynaskairys.ribbin2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
+
+        //ActionBar actionBar = getActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+}
